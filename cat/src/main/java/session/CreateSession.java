21dec1de837b2036cff2917d66490b7bb4dc989e
Wang-ze-xian <742975299@qq.com>
2019-12-05 18:13:22 +0800

    JW_201802104023_31

---
+++ cat/src/main/java/session/CreateSession.java
@@ -0,0 +1,17 @@
+package session;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/createSession")
+public class CreateSession extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setMaxInactiveInterval(30);
+        response.getWriter().println("将会在30秒后失效");
+    }
+}
---
+++ cat/src/main/java/session/GetSession.java
@@ -0,0 +1,16 @@
+package session;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/createSession")
+public class GetSession extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        response.getWriter().println(session);
+    }
+}
